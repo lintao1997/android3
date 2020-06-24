30f76eaeba61c00943899e90d650768a4f635987
longchengfeng <1181407823@qq.com>
2020-06-24 21:22:03 +0800

    aaa

---
+++ app/src/main/java/com/example/myapplication/Callbacks.java
@@ -0,0 +1,5 @@
+package com.example.myapplication;
+
+public interface Callbacks {
+    public  void onItemSelected(int id);
+}
--- app/src/main/java/com/example/myapplication/IndexActivity.java
+++ app/src/main/java/com/example/myapplication/IndexActivity.java
@@ -1,16 +1,32 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class IndexActivity extends AppCompatActivity {
+public class IndexActivity extends FragmentActivity implements Callbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
 
+    }
+
+    @Override
+    public void onItemSelected(int id) {
+        Intent intent;
+        switch (id){
+            case 0:
+                    intent = new Intent(this,ItemActivity.class);
+                    intent.putExtra("url","");
+                    startActivity(intent);
+            case 1:
+        }
 
     }
 }
