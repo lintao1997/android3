8169d103a19da91189bad38c3f159dd4d57a94a6
lintao347411 <973421666@qq.com>
2020-07-01 21:04:40 +0800

    测试增加2

--- app/src/main/java/com/example/myapplication/ItemActivity.java
+++ app/src/main/java/com/example/myapplication/ItemActivity.java
@@ -10,5 +10,10 @@ public class ItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout,ItemFragment.newInstance(1))
+        .commit();
+
+
     }
 }
